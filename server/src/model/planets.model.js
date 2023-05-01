42ec1fe09de161f217d4fbbfdc28fdc8cd5a0afe
011bq-001 <011bq-001@011bq-001s-MacBook-Air.local>
2023-05-01 23:28:25 +0530

    Added planets api. Set root package.json

--- server/src/model/planets.model.js
+++ server/src/model/planets.model.js
@@ -1,3 +1,41 @@
-const planets = ['Haha'];
+const fs = require('fs');
+const path = require('path');
+const {parse} = require('csv-parse');
 
-module.exports = planets;
+const habitablePlanets = [];
+
+function loadPlanetsData() {
+  new Promise((resolve, reject) => {
+    function isHabitablePlanet(planet) {
+      return planet['koi_disposition'] === 'CONFIRMED'
+        && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11
+        && planet['koi_prad'] < 1.6;
+    }
+    
+    fs.createReadStream(path.join(__dirname, '..', '..', 'data', 'kepler_data.csv'))
+      .pipe(parse({
+        comment: '#',
+        columns: true,
+      }))
+      .on('data', (data) => {
+        if (isHabitablePlanet(data)) {
+          habitablePlanets.push(data);
+        }
+      })
+      .on('error', (err) => {
+        console.log(err);
+        reject(err);
+      })
+      .on('end', () => {
+        console.log(`${habitablePlanets.length} habitable planets found!`);
+        resolve();
+      });
+  });
+}
+
+
+
+module.exports = {
+  loadPlanetsData,
+  planets: habitablePlanets
+};
